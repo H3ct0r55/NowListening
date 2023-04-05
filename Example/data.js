0dd38f2a5ca20cbe556a6373b875fbaac9da3b8c
Hector van der Aa <103751865+H3ct0r55@users.noreply.github.com>
2023-04-05 16:38:20 +0100

    Uploaded version 1 example files

---
+++ Example/data.js
@@ -0,0 +1,27 @@
+//Album data
+//The title of the album
+var albumTitle = 'The Seraphic Clockwork';
+//The artist of the album
+var albumArtist = 'Vanden Plas';
+
+//Streaming Links
+//Spotify Link
+var spLink = 'https://open.spotify.com/album/7wXrC7g0aC9xD1iYeOUEXq';
+//Tidal Link
+var tiLink = 'https://tidal.com/browse/album/3923714';
+//Qobuz Link
+var qoLink = 'https://www.qobuz.com/album/the-seraphic-clockwork-vanden-plas/0885686073025';
+//Apple Music Link
+var apLink = 'https://music.apple.com/us/album/the-seraphic-clockwork/372076429';
+//Deezer Link
+var deLink = 'https://www.deezer.com/en/album/559963';;
+//Amazon Music Link
+var amLink = 'https://music.amazon.com/albums/B003LQOORK';
+//YouTube Music Link
+var ytLink = 'https://music.youtube.com/playlist?list=OLAK5uy_khr2lhTUJuO_hmOrzaYz55rlRVdVW-6N8';
+
+//Appearance
+//Darkmode status (0 = always on, 1 = always off, 2 = OS dynamically defined)
+var darkmode = 2;
+//Bouncy link prompt status (true = enabled, false = disabled)
+var bouncylink = true;
